02b2b9b52e66267b7ec09d8c4199f7387b183645
zjjiang2 <zjjiang2@myseneca.ca>
2020-10-12 00:32:27 -0400

    Added JSON output Feature

--- src/cli.js
+++ src/cli.js
@@ -12,18 +12,21 @@ function versionOption(rawArgs) {
     const args = arg({
         '--version': Boolean,
         '--v': '--version',
-        '--all' : Boolean,
-        '--good' : Boolean,
-        '--bad' : Boolean
+        '--all': Boolean,
+        '--good': Boolean,
+        '--bad': Boolean,
+        '--json': Boolean,
+        '--j': '--json'
     },
-    { argv: rawArgs.slice(2)}
+        { argv: rawArgs.slice(2) }
     );
 
     return {
         showVersion: args['--version'] || false,
         showAll: args['--all'] || false,
         showGood: args['--good'] || false,
-        showBad: args['--bad'] || false
+        showBad: args['--bad'] || false,
+        showJson: args['--json'] || false
     };
 }
 
@@ -40,66 +43,116 @@ async function displayResult(data, options) {
 
     var allShowing = true;
 
-    if (options.showGood == true || options.showBad == true){
+    if (options.showGood == true || options.showBad == true) {
         allShowing = false;
     }
 
     let match = urlRegex.exec(data);
-    return new Promise((resolve, reject) => {
+    var promises = []
 
-        while (match) {
-            const siteName = match[1];
+    var goodLinksMark = 0;
 
+    while (match) {
+        const siteName = match[1];
+        goodLinksMark = goodLinks;
+        let promise = new Promise(function (resolve, reject) {
             axios({
                 method: 'GET',
                 url: siteName,
                 setTimeout: 5000,
                 validateStatus: () => true
             })
-            .then(res => {
-                if (res.status == 200) {
-                    if(options.showGood == true || allShowing == true){
-                        console.log("|Good|\t\t".bgGreen.black + siteName.green);
+                .then(res => {
+                    if (res.status == 200) {
+                        if (options.showGood == true || allShowing == true) {
+                            if (options.showJson) {
+                                resolve({ url: siteName, status: res.status });
+                            }
+                            else {
+                                console.log("|Good|\t\t".bgGreen.black + siteName.green);
+                            }
+                        }
+                        else{
+                            resolve(null)
+                        }
+                        goodLinks++;
                     }
-                    goodLinks++;
-                }
-                else if (res.status == 404 || res.status == 400) {
-                    if(options.showBad == true || allShowing == true){
-                        console.log("|Bad|\t\t".bgRed.black + siteName.red);
+                    else if (res.status == 404 || res.status == 400) {
+                        if (options.showBad == true || allShowing == true) {
+                            if (options.showJson) {
+                                resolve({ url: siteName, status: res.status });
+                            }
+                            else {
+                                console.log("|Bad|\t\t".bgRed.black + siteName.red);
+                            }
+                        }
+                        else{
+                            resolve(null)
+                        }
+                        badLinks++;
                     }
-                    badLinks++;
-                }
-                else {
-                    if(allShowing == true){
-                        console.log("|Unknown|\t".bgWhite.black + siteName.gray);
+                    else {
+                        if (allShowing == true) {
+                            if (options.showJson) {
+                                resolve({ url: siteName, status: res.status });
+                            }
+                            else {
+                                console.log("|Unknown|\t".bgWhite.black + siteName.gray);
+                            }
+                        }
+                        else{
+                            resolve(null)
+                        }
+                        unknownLinks++;
                     }
-                    unknownLinks++;
-                }
-            })
-            .catch((error) => {
-                unloadedLinks++;
-                if(allShowing == true){
-                    console.log("|Error|\t\t".bgYellow.black + siteName.yellow + "\t" + error);
-                }
-            });
+                })
+                .catch((error) => {
+                    if (allShowing == true) {
+                        if (options.showJson) {
+                            resolve({ url: siteName, status: 0 });
+                        }
+                        else {
+                            console.log("|Error|\t\t".bgYellow.black + siteName.yellow + "\t" + error);
+                        }
+                    }
+                    else{
+                        resolve(null)
+                    }
+                    unloadedLinks++;
+                })
+        })
 
-            totalLinks++;
-
-            match = urlRegex.exec(data);
-            if (!match){
-                doneMatch = true;
-            }
+        promises.push(promise);
+        
+        totalLinks++;
+        match = urlRegex.exec(data);
+        if (!match) {
+            doneMatch = true;
         }
+    }
 
-        setTimeout(() => resolve(), 0)
-    });
+    Promise
+        .all(promises)
+        .then(done => {
+            var display = [];
+            for(var i = 0; i < done.length; i++){
+                if(done[i] != null){
+                    display.push(done[i])
+                }
+            }
+            //console.log(done);
+            console.log(display);
+        })
 
 }
 
+//Main function
 export function cli(args) {
 
+    //recieve argument options
     let options = versionOption(args);
 
+    //determine output types
     if (options.showVersion == true || args.slice(2) == '/v' || args.slice(2) == '/version') {
         console.log("project-checkurl By Jason Jiang, Release 0.1, Version ".blue + version.blue);
     }
@@ -109,18 +162,18 @@ export function cli(args) {
             console.log("Please enter a file name that resides in the same directory.".red);
         }
         else if (args.length >= 3) {
-            var params = args.slice(args.length-2, args.length);;
-            var fileName = './' + args[args.length-1];
+            var params = args.slice(2);;
+            var fileName = './' + args[args.length - 1];
 
             //loop to allow the arguments to be at any
-            if(options.showGood == true || options.showBad == true){
-                for(var i = 0; i < params.length; i++){
-                    if(params[i].substring(0,2) != '--'){
+            if (args.length > 3) {
+                for (var i = 0; i < params.length; i++) {
+                    if (params[i].substring(0, 2) != '--') {
                         fileName = './' + params[i];
                     }
                 }
             }
-            
+
             const promises = []
 
             //start reading the file
@@ -128,20 +181,18 @@ export function cli(args) {
                 if (err) {
                     return console.log(err);
                 }
-                else{
-                console.log("\nValid URLs from \'".yellow + args[args.length-1].yellow + "\':\n".yellow);
+                else {
+                    console.log("\nValid URLs from \'".yellow + fileName + "\':\n".yellow);
 
-                promises.push(displayResult(data, options));
-                Promise
-                    .all(promises)
-                    .then(console.log(""))
-                    .catch(err => console.error(err));
+                    //start scanning the file, and display the results
+                    displayResult(data, options);
+
                 }
             });
+
         }
         else {
-            console.log(args[2]);
+            console.log(args);
         }
     }
-    
 }   
