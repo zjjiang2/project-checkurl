9571923033a2c28cf371aed5a2600f8a9257f23d
zjjiang2 <zjjiang2@myseneca.ca>
2020-11-14 22:47:09 -0500

    Lab 7 Works:     * added CONTRIBUTING.md     * added Prettier formats     * added ESLint checks     * added Intergration Method

---
+++ CONTRIBUTING.md
@@ -0,0 +1,25 @@
+# How to Get Started with Contributing
+Download the latest project-checkurl repository from Github:
+
+1. `git clone https://github.com/zjjiang2/project-checkurl.git`
+
+2. `cd ./project-checkurl`
+
+3. `git checkout -b BranchName`
+
+4. Get Started
+
+## Setup Prettier Formatting
+Check if the code's format is valid with:
+
+1. `npx prettier --write .`
+
+2. `npx prettier --check .`
+
+## Setup ESLint Error Check
+Use ESLint to check for any format or syntax errors:
+
+1. `npx eslint FileName`
+
+## Intergrate Prettier & ESLint into VSCode
+Simply install the "Prettier ESLint" extension on VSCode's extensions tab, make sure both Prettier & ESLint packages are installed beforehand.
--- src/cli.js
+++ src/cli.js
@@ -1,240 +1,235 @@
-
-import arg from 'arg';
-import { exit } from 'process';
-import axios from 'axios';
-import fs from 'fs'
-import colors from 'colors';
-const version = require('../package.json').version;
+import arg from "arg";
+import { exit } from "process";
+import axios from "axios";
+import fs from "fs";
+const version = require("../package.json").version;
 
 //function to handle all argument options
 function versionOption(rawArgs) {
-    const args = arg({
-        '--version': Boolean,
-        '--v': '--version',
-        '--ignore': String,
-        '--i': '--ignore',
-        '--all': Boolean,
-        '--good': Boolean,
-        '--bad': Boolean,
-        '--json': Boolean,
-        '--j': '--json',
-        '--telescope': Boolean,
-        '--t': '--telescope',
+  const args = arg(
+    {
+      "--version": Boolean,
+      "--v": "--version",
+      "--ignore": String,
+      "--i": "--ignore",
+      "--all": Boolean,
+      "--good": Boolean,
+      "--bad": Boolean,
+      "--json": Boolean,
+      "--j": "--json",
+      "--telescope": Boolean,
+      "--t": "--telescope",
     },
-        { argv: rawArgs.slice(2) }
-    );
-    return {
-        showVersion: args['--version'] || false,
-        showAll: args['--all'] || false,
-        showGood: args['--good'] || false,
-        showBad: args['--bad'] || false,
-        showJson: args['--json'] || false,
-        ignoreUrl: args['--ignore'] || '',
-        telescope: args['--telescope'] || false,
-    };
+    { argv: rawArgs.slice(2) }
+  );
+  return {
+    showVersion: args["--version"] || false,
+    showAll: args["--all"] || false,
+    showGood: args["--good"] || false,
+    showBad: args["--bad"] || false,
+    showJson: args["--json"] || false,
+    ignoreUrl: args["--ignore"] || "",
+    telescope: args["--telescope"] || false,
+  };
 }
 
 //displays the  final results
 async function displayResult(data, options, ignoreUrls = []) {
+  var regex
+  if (options.telescope) {
+    regex = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}[a-zA-Z0-9()]{1,9}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/;=]*))/g;
+  } else {
+    regex = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,9}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/;=]*))/g;
+  }
+  const urlRegex = regex;
 
-    if(options.telescope){
-        var regex = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}[a-zA-Z0-9()]{1,9}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/;=]*))/g;
-    }
-    else{
-        var regex = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,9}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/;=]*))/g;
-    }
-    const urlRegex = regex;
+  var showsAll = true;
+  if (options.showGood == true || options.showBad == true) {
+    showsAll = false;
+  }
 
-    var showsAll = true;
-    if (options.showGood == true || options.showBad == true) {
-        showsAll = false;
-    }
-    
-    let matching = urlRegex.exec(data);
-    var promises = []
+  let matching = urlRegex.exec(data);
+  var promises = [];
 
-    while (matching) {
-        const siteName = matching[1];
-        if (!ignoreUrls.some(url => siteName.startsWith(url))) {
-            let checkLinks = new Promise(function (resolve, reject) {
-                axios({
-                    method: 'GET',
-                    url: siteName,
-                    setTimeout: 5000,
-                    validateStatus: () => true
-                })
-                .then(res => {
-                    var jsonOutput = { url: siteName, status: res.status }
-                    if (res.status == 200) {
-                        if (options.showGood || showsAll) {
-                            if (options.showJson) {
-                                resolve(jsonOutput);
-                            }
-                            else {
-                                console.log("|Good|\t\t".bgGreen.black + siteName.green);
-                               }
-                            }
-                        else {
-                            resolve(null)
-                        }
-                    }
-                    else if (res.status == 404 || res.status == 400) {
-                        if (options.showBad || showsAll) {
-                            if (options.showJson) {
-                                resolve(jsonOutput);
-                            }
-                            else {
-                                console.log("|Bad|\t\t".bgRed.black + siteName.red);
-                            }
-                        }
-                        else {
-                            resolve(null)
-                        }
-                    }
-                    else {
-                        if (showsAll) {
-                            if (options.showJson) {
-                                resolve(jsonOutput);
-                            }
-                            else {
-                                console.log("|Unknown|\t".bgWhite.black + siteName.gray);
-                            }
-                        }
-                        else {
-                            resolve(null)
-                        }
-                    }
-                })
-                .catch((error) => {
-                    if (showsAll) {
-                        if (options.showJson) {
-                            resolve({ url: siteName, status: error.status });
-                        }
-                        else {
-                            console.log("|Error|\t\t".bgYellow.black + siteName.yellow + "\t" + error);
-                        }
-                    }
-                    else {
-                        resolve(null)
-                    }
-                })
-            })
-            promises.push(checkLinks);
-        }
-        matching = urlRegex.exec(data);
-    }
-
-    Promise
-        .all(promises)
-        .then(done => {
-            var display = [];
-            for (var i = 0; i < done.length; i++) {
-                if (done[i] != null) {
-                    display.push(done[i])
-                }
-            }
-            //console.log(done);
-            console.log(display);
+  while (matching) {
+    const siteName = matching[1];
+    if (!ignoreUrls.some((url) => siteName.startsWith(url))) {
+      let checkLinks = new Promise(function (resolve, reject) {
+        axios({
+          method: "GET",
+          url: siteName,
+          setTimeout: 5000,
+          validateStatus: () => true,
         })
+          .then((res) => {
+            var jsonOutput = { url: siteName, status: res.status };
+            if (res.status == 200) {
+              if (options.showGood || showsAll) {
+                if (options.showJson) {
+                  resolve(jsonOutput);
+                } else {
+                  console.log("|Good|\t\t".bgGreen.black + siteName.green);
+                }
+              } else {
+                resolve(null);
+              }
+            } else if (res.status == 404 || res.status == 400) {
+              if (options.showBad || showsAll) {
+                if (options.showJson) {
+                  resolve(jsonOutput);
+                } else {
+                  console.log("|Bad|\t\t".bgRed.black + siteName.red);
+                }
+              } else {
+                resolve(null);
+              }
+            } else {
+              if (showsAll) {
+                if (options.showJson) {
+                  resolve(jsonOutput);
+                } else {
+                  console.log("|Unknown|\t".bgWhite.black + siteName.gray);
+                }
+              } else {
+                resolve(null);
+              }
+            }
+          })
+          .catch((error) => {
+            if (showsAll) {
+              if (options.showJson) {
+                resolve({ url: siteName, status: error.status });
+              } else {
+                console.log(
+                  "|Error|\t\t".bgYellow.black + siteName.yellow + "\t" + error
+                );
+              }
+            } else {
+              resolve(null);
+            }
+          });
+      });
+      promises.push(checkLinks);
+    }
+    matching = urlRegex.exec(data);
+  }
 
+  Promise.all(promises).then((done) => {
+    var display = [];
+    for (var i = 0; i < done.length; i++) {
+      if (done[i] != null) {
+        display.push(done[i]);
+      }
+    }
+    //console.log(done);
+    console.log(display);
+  });
 }
 
 //Main function
 export function cli(args) {
+  //recieve argument options
+  let options = versionOption(args);
 
-    //recieve argument options
-    let options = versionOption(args);
+  //determine output types
+  if (
+    options.showVersion == true ||
+    args.slice(2) == "/v" ||
+    args.slice(2) == "/version"
+  ) {
+    console.log(
+      "project-checkurl By Jason Jiang, Release 0.1, Version ".blue +
+        version.blue
+    );
+  } else {
+    if (args.length == 2) {
+      console.log(
+        "Please enter a file name that resides in the same directory.".red
+      );
+    } else if (args.length >= 3) {
+      var params = args.slice(2);
 
-    //determine output types
-    if (options.showVersion == true || args.slice(2) == '/v' || args.slice(2) == '/version') {
-        console.log("project-checkurl By Jason Jiang, Release 0.1, Version ".blue + version.blue);
-    }
-    else {
+      if (options.telescope) {
+        var urlName = args[args.length - 1];
 
-        if (args.length == 2) {
-            console.log("Please enter a file name that resides in the same directory.".red);
-        }
-        else if (args.length >= 3) {
-            var params = args.slice(2);
-
-            if(options.telescope){
-                var urlName = args[args.length - 1];
-
-                //loop to allow the arguments to be at any position
-                if (args.length > 3) {
-                    for (var i = 0; i < params.length; i++) {
-                        if (params[i].substring(0, 2) != '--') {
-                            urlName = params[i];
-                        }
-                    }
-                }   
-
-                //access the argument url
-                axios({
-                    method: 'GET',
-                    url: urlName,
-                    setTimeout: 5000,
-                    validateStatus: () => true
-                })
-                .then(obj => {
-                    var data = '';
-                    //store data url into string
-                    for(var i = 0; i < obj.data.length; i++){
-                        data = data + urlName + obj.data[i].url.slice(6) + '\n';
-                    }
-                    displayResult(data, options);
-                })
-                .catch(err =>{
-                    console.log("Invalid URL");
-                });
-
+        //loop to allow the arguments to be at any position
+        if (args.length > 3) {
+          for (var i = 0; i < params.length; i++) {
+            if (params[i].substring(0, 2) != "--") {
+              urlName = params[i];
             }
-            else {
-                const ignoreFile = options.ignoreUrl;
+          }
+        }
 
-                var params = args.slice(2);
-                var fileName = './' + args[args.length - 1];
-
-                //loop to allow the arguments to be at any position
-                if (args.length > 3) {
-                    for (var i = 0; i < params.length; i++) {
-                        if (params[i].substring(0, 2) != '--') {
-                            fileName = './' + params[i];
-                        }
-                    }
-                }
-
-                //start reading the file
-                var ignoreList = [];
-                console.log(ignoreFile)
-                if (ignoreFile) {
-                    fs.readFile(ignoreFile, 'utf8', function (err, ignoreUrls) {
-                        if (err) {
-                            return console.log(err);
-                        }
-                        console.log("\nValid URLs from \'".yellow + fileName + "\':\n".yellow);
-                        ignoreUrls = ignoreUrls.split('\n').filter(e => !e.startsWith("#"))
-                        ignoreUrls.forEach(e => {
-                            if (!['http://', 'https://'].some(http => e.startsWith(http))) {
-                                console.log('Invaild urls. Please starts with "http:// or https://"');
-                                exit(1);
-                            }
-                        });
-                        ignoreList = ignoreUrls;
-                    });
-                }
-                fs.readFile(fileName, 'utf8', function (err, data) {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    console.log("\nValid URLs from \'".yellow + fileName + "\':\n".yellow);
-                    //start scanning the file, and display the results
-                    displayResult(data, options, ignoreList);
-                });
+        //access the argument url
+        axios({
+          method: "GET",
+          url: urlName,
+          setTimeout: 5000,
+          validateStatus: () => true,
+        })
+          .then((obj) => {
+            var data = "";
+            //store data url into string
+            for (var i = 0; i < obj.data.length; i++) {
+              data = data + urlName + obj.data[i].url.slice(6) + "\n";
             }
+            displayResult(data, options);
+          })
+          .catch((err) => {
+            console.log("Invalid URL, Error:" + err);
+          });
+      } else {
+        const ignoreFile = options.ignoreUrl;
+
+        var params = args.slice(2);
+        var fileName = "./" + args[args.length - 1];
+
+        //loop to allow the arguments to be at any position
+        if (args.length > 3) {
+          for (var i = 0; i < params.length; i++) {
+            if (params[i].substring(0, 2) != "--") {
+              fileName = "./" + params[i];
+            }
+          }
         }
-        else {
-            console.log(args);
+
+        //start reading the file
+        var ignoreList = [];
+        console.log(ignoreFile);
+        if (ignoreFile) {
+          fs.readFile(ignoreFile, "utf8", function (err, ignoreUrls) {
+            if (err) {
+              return console.log(err);
+            }
+            console.log(
+              "\nValid URLs from '".yellow + fileName + "':\n".yellow
+            );
+            ignoreUrls = ignoreUrls
+              .split("\n")
+              .filter((e) => !e.startsWith("#"));
+            ignoreUrls.forEach((e) => {
+              if (!["http://", "https://"].some((http) => e.startsWith(http))) {
+                console.log(
+                  'Invaild urls. Please starts with "http:// or https://"'
+                );
+                exit(1);
+              }
+            });
+            ignoreList = ignoreUrls;
+          });
         }
+        fs.readFile(fileName, "utf8", function (err, data) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log("\nValid URLs from '".yellow + fileName + "':\n".yellow);
+          //start scanning the file, and display the results
+          displayResult(data, options, ignoreList);
+        });
+      }
+    } else {
+      console.log(args);
     }
-}   
+  }
+}
